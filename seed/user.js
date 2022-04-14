93bfeb122774240c61bc9d073449b2ca07742ddc
Raul Veras <verasraul@gmail.com>
2022-04-13 22:58:40 -0400

    updated seed/user

--- seed/user.js
+++ seed/user.js
@@ -1,4 +1,6 @@
 const db = require('../db')
+const { faker } = require('@faker-js/faker');
+
 
 const User = require('../models/user');
 
@@ -6,17 +8,23 @@ db.on('error', console.error.bind(console, "MongoDB connection error:"));
 
 
 const main = async () => {
-    const users = [
-    { name: "Benny", age: 28, status: "active" },
-    { name: "Claire", age: 28, status: "active" },
-    { name: "Joey", age: 28, status: "active" },
-    { name: "Abe", age: 22, status: "pending" },
-    { name: "Sunny", age: 23, status: "pending" },
-    { name: "Lizzy", age: 28, status: "active" },
-    { name: "Julie", age: 21, status: "active" },
-    ];
-await User.insertMany(users);
-console.log('Created users!');
+    const employees = [...Array(100)].map(employee => (
+        {
+            first_name: faker.name.firstName(),
+            last_name: faker.name.lastName(),
+            email: faker.internet.email(),
+            job_title: faker.name.jobTitle(),
+            address: {
+                street: faker.address.streetName(),
+                city: faker.address.city(),
+                state: faker.address.state(),
+                zip: faker.address.zipCode()
+            }
+        }
+    ))
+    
+await User.insertMany(employees);
+console.log('Created employees!');
 };
 
 const run = async () => {
